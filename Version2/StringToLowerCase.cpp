a42fac9ae159276f63626ffe19cb15c00ef104b8
UttaraKulkarni <uttaravishwas.kulkarni@sjsu.edu>
2018-08-24 14:28:54 -0700

    C++ code to convert any string into lowercase

---
+++ Version2/StringToLowerCase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string toLowerCase(string str) {
+
+for (int i=0; i<str.length(); i++)
+{
+  if (int(str[i])>=65 && int(str[i])<=90) {
+    str[i]=char(int(str[i])+32);
+  }
+}
+return str;
+}
+
+int main()
+{
+  string inputString = "Hello";
+  string outputString=toLowerCase(inputString);
+  cout<<outputString<<endl;
+}
