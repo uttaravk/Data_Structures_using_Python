f90c22ea3dfd16a7ccd0bf7e5b01dafe82c3f22a
UttaraKulkarni <uttaravishwas.kulkarni@sjsu.edu>
2018-10-26 08:55:26 -0700

    python solution for maximum subarray and perfect number problems

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,51 +1,57 @@
 """
-We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
 Example:
 
-Input: 28
-Output: True
-Explanation: 28 = 1 + 2 + 4 + 7 + 14
-Note: The input number n will not exceed 100,000,000. (1e8)
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 """
+import sys
 
-# Method 1
-
-
-# This method will not work for large numbers
-def checkPerfectNumberMethod1(num):
-    i = 1
-    mysum = 0
-    while i < num:
-        if (num % i == 0):
-            mysum += i
-        i = i + 1
-    if mysum == num:
-        return True
-    return False
+# Brute Force
 
 
-# Method 2
+def maxSubArrayMethod1(nums):
+    if not nums:
+        return 0
+    sum = min(nums)
+    count = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i] >= 0:
+            if count < 0:
+                count = nums[i]
+            else:
+                count = count + nums[i]
+            if count > sum:
+                sum = count
+        elif nums[i] < 0:
+            if nums[i] >= count:
+                count = nums[i]
+            else:
+                if count > 0:
+                    count = count + nums[i]
+            if count > sum:
+                sum = count
+    if sum < max(nums):
+        sum = max(nums)
+    return sum
+
+# Using system module
 
 
-# This method will work for large numbers as well
-def checkPerfectNumberMethod2(num):
-    if num <= 0:
-        return False
-    i = 1
-    mysum = 0
-    while i * i <= num:
-        if (num % i == 0):
-            mysum += i
-            if i * i != num:
-                mysum += num / i
-        i = i + 1
-    if mysum - num == num:
-        return True
-    return False
+def maxSubArrayMethod2(nums):
+    result = -sys.maxint
+    sum = 0
+    for i in range(len(nums)):
+        sum = max(nums[i], sum + nums[i])
+        result = max(result, sum)
+    return result
 
 
-# Function Call
-print(checkPerfectNumberMethod1(28))
-print(checkPerfectNumberMethod2(28))
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArrayMethod1(nums))
+print(maxSubArrayMethod2(nums))
