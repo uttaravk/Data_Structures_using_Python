57e9a3f09519818d8966bb845412a9136be3d883
UttaraKulkarni <uttaravishwas.kulkarni@sjsu.edu>
2018-11-24 19:51:11 -0800

    two solutions to arrange the coins in a pattern

---
+++ arrange_coins.py
@@ -0,0 +1,54 @@
+"""
+You have a total of n coins that you want to form
+in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+.
+. .
+. .
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+.
+. .
+. . .
+. .
+
+Because the 4th row is incomplete, we return 3.
+"""
+
+n = 8
+
+
+def arrange_coins_method1(n):
+    if n < 2:
+        return n
+    row = 0
+    while n > 0:
+        row += 1
+        n -= (row)
+    if n == 0:
+        return row
+    return row - 1
+
+
+def arrange_coins_method2(n):
+    if n < 2:
+        return n
+    return (int(pow(8 * n + 1, 0.5)) - 1) / 2
+
+
+print(arrange_coins_method1(n))
+print(arrange_coins_method2(n))
